7fb4f5bd648c04f7dc6e6304006c7f05fde3a0aa
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-01-30 10:23:48 -0300

    configurando o projeto e realizando teste de stress no uso do listview

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -2,15 +2,36 @@ package br.com.alura.ceep.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Notas");
         setContentView(R.layout.lista_notas_activity);
-        setTitle("CEEP");
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        for(int i = 0 ; i <= 20000; i++){
+            dao.insere(new Nota("Nota de numero: "+i, "descricao de numero: "+i));
+        }
+
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+
+
+
     }
 }
